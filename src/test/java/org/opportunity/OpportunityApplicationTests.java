54903fe46a76317575b5d155707f15bfaefe09ee
oxcar103 <oxcar103@gmail.com>
2022-11-28 15:51:33 +0100

    Auto-generated code

---
+++ src/test/java/org/opportunity/OpportunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opportunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpportunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
