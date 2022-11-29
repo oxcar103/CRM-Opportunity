d2bc8526db52028c7e6b6470776b99f26b20766a
oxcar103 <oxcar103@gmail.com>
2022-11-29 16:14:30 +0100

    Second Test: Incorrect Date (Fail)

--- src/test/java/org/opportunity/OpportunityApplicationTests.java
+++ src/test/java/org/opportunity/OpportunityApplicationTests.java
@@ -1,15 +1,17 @@
 package org.opportunity;
 
+import org.aspectj.weaver.bcel.ExceptionRange;
 import org.junit.jupiter.api.Test;
 import org.opportunity.entity.Action;
+import org.opportunity.entity.InvalidDateException;
 import org.opportunity.entity.VIA;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.Objects;
+
+import static org.assertj.core.api.Fail.fail;
 
 @SpringBootTest
 class OpportunityApplicationTests {
@@ -19,7 +21,7 @@ class OpportunityApplicationTests {
 	}
 
 	@Test
-	void Action_FutureDate_CorrectInitialization(){
+	void Action_FutureDate_CorrectInitialization() throws Exception {
 		LocalDate date = LocalDate.now();
 		Action action = new Action("Juan", VIA.EMAIL, date.plusDays(1));
 
@@ -28,4 +30,18 @@ class OpportunityApplicationTests {
 		assert action.getVia() == VIA.EMAIL;
 		assert Objects.equals(date.plusDays(1), action.getDate());
 	}
+
+
+	@Test
+	void Action_PastDate_Exception() throws InvalidDateException {
+		try {
+			LocalDate date = LocalDate.now();
+			new Action("Juan", VIA.EMAIL, date.minusDays(1));
+			assert false;
+		} catch (InvalidDateException e){
+			assert true;
+		}
+
+	}
+
 }
