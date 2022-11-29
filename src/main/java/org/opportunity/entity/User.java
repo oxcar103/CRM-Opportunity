be0eeb3c57868bd63f73f98181fb1c3411e6d017
oxcar103 <oxcar103@gmail.com>
2022-11-29 13:24:09 +0100

    User class

--- src/main/java/org/opportunity/entity/User.java
+++ src/main/java/org/opportunity/entity/User.java
@@ -6,4 +6,34 @@ public class User {
     public String username;
     public String password;
     List<Opportunity> opportunities;
+
+    public User(String username, String password, List<Opportunity> opportunities) {
+        this.username = username;
+        this.password = password;
+        this.opportunities = opportunities;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Opportunity> getOpportunities() {
+        return opportunities;
+    }
+
+    public void setOpportunities(List<Opportunity> opportunities) {
+        this.opportunities = opportunities;
+    }
 }
